493313b65d5edc0da5f5a322a9362a1c88543ea9
lidiacollantes <lydialaoveja@gmail.com>
2019-10-05 16:48:15 +0200

    Mi aplicación Django Girls, primer commit

---
+++ imprime_alumnos.py
@@ -0,0 +1,8 @@
+print("Hello, Django girls!")
+alumno1= {"nombre":"Marta", "apellido":"Perez", "edad": "30"}
+alumno2= {"nombre":"Bego", "apellido":"Martin", "edad": "28"}
+alumnos= [alumno1, alumno2]
+print (alumnos)
+alumno1 ["ciudad"]= "Sevilla"
+alumno2 ["ciudad"]= "Jerez"
+print (alumnos)
---
+++ python_intro.py
@@ -0,0 +1,7 @@
+nombre = "Marta"
+if nombre == "Marta":
+    print ("Hola Marta!")
+elif nombre == "Lydia":
+    print("hola Lydia")
+else:
+    print("hola anonymous!")
